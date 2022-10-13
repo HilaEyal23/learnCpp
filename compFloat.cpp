bf818cfc97cfc25ec6b0df7226525bd09e541d58
Hila Eyal <96479692+HilaEyal23@users.noreply.github.com>
2022-10-14 01:24:52 +0300

    Create compFloat.cpp

---
+++ compFloat.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
+// return true if the difference between a and b is within epsilon percent of the larger of a and b
+bool approximatelyEqualRel(double a, double b, double relEpsilon)
+{
+	return (std::abs(a - b) <= (std::max(std::abs(a), std::abs(b)) * relEpsilon));
+}
+
+bool approximatelyEqualAbsRel(double a, double b, double absEpsilon, double relEpsilon)
+{
+    // Check if the numbers are really close -- needed when comparing numbers near zero.
+    double diff{ std::abs(a - b) };
+    if (diff <= absEpsilon)
+        return true;
+
+    // Otherwise fall back to Knuth's algorithm
+    return (diff <= (std::max(std::abs(a), std::abs(b)) * relEpsilon));
+}
