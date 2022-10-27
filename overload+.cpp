833f15a65618710f4470cd3612fba6dea2a52048
Hila Eyal <96479692+HilaEyal23@users.noreply.github.com>
2022-10-27 16:19:40 +0300

    Create overload+.cpp

---
+++ overload+.cpp
@@ -0,0 +1,30 @@
+//In case you have defined scoped enumerations and you find yourself often explicitly converting a scoped enumeration to an integer 
+//by using a static_cast.
+
+#include <iostream>
+
+enum class Animals
+{
+    chicken, // 0
+    dog, // 1
+    cat, // 2
+    elephant, // 3
+    duck, // 4
+    snake, // 5
+
+    maxAnimals,
+};
+
+// Overload the unary + operator to convert Animals to the underlying type
+// adapted from https://stackoverflow.com/a/42198760, thanks to Pixelchemist for the idea
+constexpr auto operator+(Animals a) noexcept
+{
+    return static_cast<std::underlying_type_t<Animals>>(a);
+}
+
+int main()
+{
+    std::cout << +Animals::elephant << '\n'; // convert Animals::elephant to an integer using unary operator+
+
+    return 0;
+}
